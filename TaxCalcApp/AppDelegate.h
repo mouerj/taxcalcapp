57334d2b1e55c6c51baac3a6f8533fc5d153d9f1
mouerj <joseph.mouer@icloud.com>
2016-01-02 15:57:13 -0500

    Initial Commit

---
+++ TaxCalcApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TaxCalcApp
+//
+//  Created by Joseph Mouer on 1/2/16.
+//  Copyright © 2016 Joseph Mouer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
